32c56c7e7744291936138edd5994e6dde44ce2d7
nnobinelo <40398714+nnobinelo@users.noreply.github.com>
2018-08-22 01:55:08 -0400

    Added a test admob banner and updates shop more

--- DashGame/Assets/Scripts/ShopController.cs
+++ DashGame/Assets/Scripts/ShopController.cs
@@ -10,10 +10,17 @@ public class ShopController : MonoBehaviour
     public Button viewBallsButton;
     public Button viewPaddlesButton;
     public Button viewIAPButton;
+    public Button selectButton;
+    public GameObject selectedIcon;
+    public Button unlockButton;
+    public Button purchaseButton;
 
+    string currentMenu = null;
+    Text unlockButtonGemCost;
     GameObject ballSelectionMenu;
     GameObject paddleSelectionMenu;
     GameObject IAPmenu;
+    SnapScrollRectController ballScrollRect, paddleScrollRect;
 
     public enum buttonLayout { selected, unlocked, locked }
 
@@ -24,29 +31,45 @@ public class ShopController : MonoBehaviour
         ballSelectionMenu = transform.Find("BallScollPanel").gameObject;
         paddleSelectionMenu = transform.Find("PaddleScollPanel").gameObject;
         IAPmenu = transform.Find("IAP").gameObject;
+
+        ballScrollRect = ballSelectionMenu.GetComponent<SnapScrollRectController>();
+        paddleScrollRect = paddleSelectionMenu.GetComponent<SnapScrollRectController>();
+        unlockButtonGemCost = unlockButton.GetComponentInChildren<Text>();
     }
 
-    public void SetButtonLayout(SnapScrollRectController.ShopItem item)
+    public void SetButtonLayout(SnapScrollRectController.ShopItem item,int gemCost)
     {
         switch (item.buttonLayout)
         {
             case buttonLayout.selected:
-
+                selectedIcon.SetActive(true);
+                selectButton.gameObject.SetActive(false);
+                unlockButton.gameObject.SetActive(false);
+                purchaseButton.gameObject.SetActive(false);
                 break;
 
             case buttonLayout.unlocked:
-
+                selectedIcon.SetActive(false);
+                selectButton.gameObject.SetActive(true);
+                unlockButton.gameObject.SetActive(false);
+                purchaseButton.gameObject.SetActive(false);
                 break;
 
             case buttonLayout.locked:
+                selectedIcon.SetActive(false);
+                selectButton.gameObject.SetActive(false);
+                unlockButton.gameObject.SetActive(true);
+                purchaseButton.gameObject.SetActive(true);
 
+                unlockButtonGemCost.text = gemCost.ToString();
                 break;
-
         }
     }
 
     public void GoToBallSelection()
     {
+        currentMenu = "ball";
+
         viewBallsButton.interactable = false;
         viewPaddlesButton.interactable = true;
         viewIAPButton.interactable = true;
@@ -54,10 +77,15 @@ public class ShopController : MonoBehaviour
         ballSelectionMenu.SetActive(true);
         paddleSelectionMenu.SetActive(false);
         IAPmenu.SetActive(false);
+
+        ballScrollRect.Go2Shop();
+        ballScrollRect.Go2Selected();
     }
 
     public void GoToPaddleSelection()
     {
+        currentMenu = "paddle";
+
         viewBallsButton.interactable = true;
         viewPaddlesButton.interactable = false;
         viewIAPButton.interactable = true;
@@ -65,13 +93,23 @@ public class ShopController : MonoBehaviour
         ballSelectionMenu.SetActive(false);
         paddleSelectionMenu.SetActive(true);
         IAPmenu.SetActive(false);
+
+        paddleScrollRect.Go2Shop();
+        paddleScrollRect.Go2Selected();
     }
 
     public void GoToIAP()
     {
+        currentMenu = "IAP";
+
         viewBallsButton.interactable = true;
         viewPaddlesButton.interactable = true;
         viewIAPButton.interactable = false;
+
+        selectedIcon.SetActive(false);
+        selectButton.gameObject.SetActive(false);
+        unlockButton.gameObject.SetActive(false);
+        purchaseButton.gameObject.SetActive(false);
 
         ballSelectionMenu.SetActive(false);
         paddleSelectionMenu.SetActive(false);
@@ -80,6 +118,8 @@ public class ShopController : MonoBehaviour
 
     public void Go2Shop()
     {
+        currentMenu = "ball";
+
         viewBallsButton.interactable = false;
         viewPaddlesButton.interactable = true;
         viewIAPButton.interactable = true;
@@ -87,6 +127,45 @@ public class ShopController : MonoBehaviour
         ballSelectionMenu.SetActive(true);
         paddleSelectionMenu.SetActive(false);
         IAPmenu.SetActive(false);
+
+        ballScrollRect.Go2Shop();
+        ballScrollRect.Go2Selected();
+    }
+
+    public void SelectItem()
+    {
+        if(currentMenu == "ball")
+        {
+            ballScrollRect.SelectItem();
+        }
+        else if(currentMenu == "paddle")
+        {
+            paddleScrollRect.SelectItem();
+        }
+    }
+
+    public void BuyItem()
+    {
+        if (currentMenu == "ball")
+        {
+            ballScrollRect.SelectItem();
+        }
+        else if (currentMenu == "paddle")
+        {
+            paddleScrollRect.SelectItem();
+        }
+    }
+
+    public void UnlockItem()
+    {
+        if (currentMenu == "ball")
+        {
+            ballScrollRect.SelectItem();
+        }
+        else if (currentMenu == "paddle")
+        {
+            paddleScrollRect.SelectItem();
+        }
     }
 
 }
